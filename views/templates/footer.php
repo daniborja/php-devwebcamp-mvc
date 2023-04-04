d9bbba09abdb1c59c1a0a152fd79d325a0e873d6
Alex Martin <alextravc.342@gmail.com>
2023-04-04 07:51:46 -0500

    [ADD]: footer design

---
+++ views/templates/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+    <div class="footer__grid">
+        <div class="footer__content">
+            <h3 class="footer__logo">&#60;DevWebCamp /></h3>
+
+            <p class="footer__text">
+                DevWebCamp es una conferencia para desarrolladores de todos los niveles, se lleva a cabo de forma presencial y en línea.
+            </p>
+        </div>
+
+        <nav class="social-menu">
+            <a class="social-menu__link" id="facebook" rel="noopener noreferrer" target="_blank" href="https://github.com/AlexMartin998">
+                <!-- Accesible: -->
+                <span class="social-menu__hidden">Facebook</span>
+            </a>
+            <a class="social-menu__link" id="twitter" rel="noopener noreferrer" target="_blank" href="https://github.com/AlexMartin998">
+                <span class="social-menu__hidden">Twitter</span>
+            </a>
+            <a class="social-menu__link" id="youtube" rel="noopener noreferrer" target="_blank" href="https://github.com/AlexMartin998">
+                <span class="social-menu__hidden">YouTube</span>
+            </a>
+            <a class="social-menu__link" id="instagram" rel="noopener noreferrer" target="_blank" href="https://github.com/AlexMartin998">
+                <span class="social-menu__hidden">Instagram</span>
+            </a>
+            <a class="social-menu__link" id="tiktok" rel="noopener noreferrer" target="_blank" href="https://github.com/AlexMartin998">
+                <span class="social-menu__hidden">Tiktok</span>
+            </a>
+            <a class="social-menu__link" id="github" rel="noopener noreferrer" target="_blank" href="https://github.com/AlexMartin998">
+                <span class="social-menu__hidden">GitHub</span>
+            </a>
+        </nav>
+    </div>
+
+    <p class="footer__copyright">DevWebCamp
+        <span class="footer__copyright--regular">
+            - Todos los derechos reservados <?php echo date('Y') ?>
+        </span>
+    </p>
+</footer>
