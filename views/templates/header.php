2b0a3080129d01dccd54627a71fa741db3ac7e49
Alex Martin <alextravc.342@gmail.com>
2023-04-03 22:04:01 -0500

    [ADD]: header design + responsive text with clamp

---
+++ views/templates/header.php
@@ -0,0 +1,19 @@
+<header class="header">
+    <div class="header__container">
+        <nav class="header__navigation">
+            <a href="/register" class="header__link">Registro</a>
+            <a href="/login" class="header__link">Login</a>
+        </nav>
+
+        <div class="header__content">
+            <a href="/">
+                <h1 class="header__logo">&#60;DevWebCamp /></h1>
+            </a>
+
+            <p class="header__text">Octubre 5-6 - 2023</p>
+            <p class="header__text header__text--mode">En Línea - Presencial</p>
+
+            <a href="/registro" class="header__button">Comprar Pase</a>
+        </div>
+    </div>
+</header>
