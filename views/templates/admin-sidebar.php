45a81404deb1540935398283c0d3fec495c22dc8
Alex Martin <alextravc.342@gmail.com>
2023-04-05 08:23:09 -0500

    [ADD]: sidebar design

---
+++ views/templates/admin-sidebar.php
@@ -0,0 +1,28 @@
+<aside class="dashboard__sidebar">
+    <nav class="dashboard__menu">
+        <a href="/admin/dashboard" class="dashboard__link">
+            <i class="fa-solid fa-house dashboard__icon"></i>
+            <span class="dashboard__menu-text">Inicio</span>
+        </a>
+
+        <a href="/admin/ponentes" class="dashboard__link">
+            <i class="fa-solid fa-microphone dashboard__icon"></i>
+            <span class="dashboard__menu-text">Ponentes</span>
+        </a>
+
+        <a href="/admin/eventos" class="dashboard__link">
+            <i class="fa-solid fa-calendar dashboard__icon"></i>
+            <span class="dashboard__menu-text">Eventos</span>
+        </a>
+
+        <a href="/admin/registrados" class="dashboard__link">
+            <i class="fa-solid fa-user dashboard__icon"></i>
+            <span class="dashboard__menu-text">Registrados</span>
+        </a>
+
+        <a href="/admin/regalos" class="dashboard__link">
+            <i class="fa-solid fa-gift dashboard__icon"></i>
+            <span class="dashboard__menu-text">Regalos</span>
+        </a>
+    </nav>
+</aside>
