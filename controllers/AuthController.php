a0acd83fe72990cc146e72176dd6a2c41504ff8f
Alex Martin <alextravc.342@gmail.com>
2023-04-04 21:57:20 -0500

    [ADD]: logout

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -48,9 +48,11 @@ class AuthController
 
     public static function logout()
     {
-        session_start();
-        $_SESSION = [];
-        header('Location: /');
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            session_start();
+            $_SESSION = [];
+            header('Location: /');
+        }
     }
 
     public static function register(Router $router)
