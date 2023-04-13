26f3bba0a7f9d8440014ce09940a4e6b250f660c
Alex Martin <alextravc.342@gmail.com>
2023-04-13 10:45:35 -0500

    [ADD]: conference events card design

--- views/pages/conferences.php
+++ views/pages/conferences.php
@@ -14,15 +14,13 @@
                     <div class="event__information">
                         <h4 class="event__name"><?php echo $event->name ?></h4>
 
-                        <div>
-                            <p class="event__information"><?php echo $event->description ?></p>
-                        </div>
+                        <p class="event__introduction"><?php echo $event->description ?></p>
 
                         <div class="event__author-info">
                             <picture>
                                 <source srcset="/img/speakers/<?php echo $event->speaker->image; ?>.webp" type="image/webp" draggable="false">
                                 <source srcset="/img/speakers/<?php echo $event->speaker->image; ?>.png" type="image/png" draggable="false">
-                                <img src="/img/speakers/<?php echo $event->speaker->image; ?>.png" alt="Imagen Ponente" draggable="false">
+                                <img loading="lazy" src="/img/speakers/<?php echo $event->speaker->image; ?>.png" alt="Imagen Ponente" width="200" height="300" class="event__author-image" draggable="false">
                             </picture>
 
                             <p class="event__author-name">
