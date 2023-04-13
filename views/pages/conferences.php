96e7fb88fa91c142b1d7604ecc885f81dff60337
Alex Martin <alextravc.342@gmail.com>
2023-04-13 09:14:34 -0500

    [ADD]: conferences page design (bloc modifier in sass)

---
+++ views/pages/conferences.php
@@ -0,0 +1,32 @@
+<main class="schedule">
+    <h2 class="schedule__heading"><?php echo $title; ?></h2>
+    <p class="schedule__description">Talletes y Conferencias dictados por expertos en desarrollo web</p>
+
+    <div class="events">
+        <h3 class="events__heading">&lt;Conferencias /></h3>
+
+        <p class="events__date">Viernes 5 de Octubre</p>
+        <div class="events__list">
+
+        </div>
+
+        <p class="events__date">Sábado 6 de Octubre</p>
+        <div class="events__list">
+
+        </div>
+    </div>
+
+    <div class="events events--workshops">
+        <h3 class="events__heading">&lt;Workshops /></h3>
+
+        <p class="events__date">Viernes 5 de Octubre</p>
+        <div class="events__list">
+
+        </div>
+
+        <p class="events__date">Sábado 6 de Octubre</p>
+        <div class="events__list">
+
+        </div>
+    </div>
+</main>
