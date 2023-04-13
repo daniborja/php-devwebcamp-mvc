6ad1b39852523afc5d642accc52e9131ecc1a5eb
Alex Martin <alextravc.342@gmail.com>
2023-04-13 08:57:37 -0500

    [ADD]: packages page design

---
+++ views/pages/packages.php
@@ -0,0 +1,45 @@
+<main class="packages">
+    <h2 class="packages__heading"><?php echo $title; ?></h2>
+    <p class="packages__description">Compara los paqueted de DevWebCamp</p>
+
+    <div class="packages__grid">
+        <div class="package">
+            <h3 class="package__name">
+                Pase Gratis
+            </h3>
+            <ul class="package__list">
+                <li class="package__item">
+                    Acceso Virtual a DevWebCamp
+                </li>
+            </ul>
+            <p class="package__price">$0</p>
+        </div>
+
+        <div class="package">
+            <h3 class="package__name">
+                Pase Presencial
+            </h3>
+            <ul class="package__list">
+                <li class="package__item">Acceso Presencial a DevWebCamp</li>
+                <li class="package__item">Pase por 2 días</li>
+                <li class="package__item">Acceso a talleres y conferencias</li>
+                <li class="package__item">Acceso a las grabaciones</li>
+                <li class="package__item">Camisa del Evento</li>
+                <li class="package__item">Comida y Bebida</li>
+            </ul>
+            <p class="package__price">$199</p>
+        </div>
+
+        <div class="package">
+            <h3 class="package__name">Pase Virtual</h3>
+            <ul class="package__list">
+                <li class="package__item">Acceso Virtual a DevWebCamp</li>
+                <li class="package__item">Pase por 2 días</li>
+                <li class="package__item">Acceso a talleres y conferencias</li>
+                <li class="package__item">Acceso a las grabaciones</li>
+            </ul>
+            <p class="package__price">$49</p>
+        </div>
+    </div>
+
+</main>
