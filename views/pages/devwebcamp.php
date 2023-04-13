8d35b540c8990c4c69a9aa18ab62e056a5d52ff5
Alex Martin <alextravc.342@gmail.com>
2023-04-13 07:49:03 -0500

    [ADD]: events public page design

---
+++ views/pages/devwebcamp.php
@@ -0,0 +1,20 @@
+<main class="devwebcamp">
+    <h2 class="devwebcamp__heading"><?php echo $title; ?></h2>
+
+    <p class="devwebcamp__description">Conoce la conferencia más importante de Latinoamérica</p>
+
+    <div class="devwebcamp__grid">
+        <div class="devwebcamp__image">
+            <picture>
+                <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
+                <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+                <img loading="lazy" src="build/img/sobre_devwebcamp.jpg" alt="Imagen DevWebCamp" width="200" height="300">
+            </picture>
+        </div>
+
+        <div class="devwebcamp__content">
+            <p class="devwebcamp__text">Aliquam est elit, condimentum a justo vitae, fringilla dignissim tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam quis lorem eget felis dignissim dignissim. Praesent ac lorem convallis, ultrices neque quis, cursus lorem.</p>
+            <p class="devwebcamp__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, ex quis ea autem aspernatur maxime eos? In distinctio repellendus maxime quos qui. Iste, doloremque mollitia? Fugit praesentium a facere illum architecto aliquam inventore!</p>
+        </div>
+    </div>
+</main>
